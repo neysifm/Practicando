38f211a6823c0844ce65a0abf2b2e6bfe38ac6a0
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-05-29 09:30:09 -0400

    Registro de Productos

--- RegistroProductos/MainForm.Designer.cs
+++ RegistroProductos/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegistroProductos
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(440, 267);
+            this.ClientSize = new System.Drawing.Size(459, 280);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
